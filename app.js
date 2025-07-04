f5a6728c4244aed1fcf8428dec0d8c8fa4a73743
EC2 Default User <ec2-user@ip-10-0-1-109.ec2.internal>
2025-07-04 17:14:12 +0000

    Added Prometheus metrics endpoint

--- app.js
+++ app.js
@@ -1,10 +1,31 @@
 const express = require('express');
+const client = require('prom-client');
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('🚀 Hello Saed!, your app just auto-deployed via GitHub Actions!');
+// Create a registry to register the metrics
+const register = new client.Registry();
+client.collectDefaultMetrics({ register });
+
+// Define your custom counter (optional)
+const httpRequestCounter = new client.Counter({
+  name: 'http_requests_total',
+  help: 'Total number of HTTP requests',
 });
+register.registerMetric(httpRequestCounter);
+
+// Root endpoint
+app.get('/', (req, res) => {
+  httpRequestCounter.inc();
+  res.send('🚀 Hello Ridwan!!, your app just auto-deployed via GitHub Actions!');
+});
+
+// Metrics endpoint
+app.get('/metrics', async (req, res) => {
+  res.set('Content-Type', register.contentType);
+  res.end(await register.metrics());
+});
+
 app.listen(port, () => {
   console.log(`App listening at http://localhost:${port}`);
 });
